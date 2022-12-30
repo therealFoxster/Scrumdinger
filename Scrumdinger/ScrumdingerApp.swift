cb8b1eae41bac5ade9971eff0e0806c258970854
foxster-mp4 <foxster.utube@gmail.com>
2022-12-29 21:34:34 -0800

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Huy Bui on 2022-12-29.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
