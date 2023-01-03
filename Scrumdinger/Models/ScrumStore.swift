853c34d753d1072375a32f4103ebc253bac579d4
foxster-mp4 <foxster.utube@gmail.com>
2023-01-02 22:15:29 -0800

    Modern async interfaces

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -19,6 +19,23 @@ class ScrumStore: ObservableObject { // ObservableObject: class-constrained prot
             .appendingPathComponent("scrums.data")
     }
     
+    // Async interface for legacy load method.
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { // Suspend load function, then pass continuation to closure.
+            continuation in // continuation: value that represent code after an awaited function.
+            load { result in
+                // Must call continuation's resume() exactly once on every possible execution path.
+                switch result {
+                case .success(let scrums):
+                    continuation.resume(returning: scrums) // Resume the task awaiting continuation by having it return normally from its suspension point.
+                case .failure(let error):
+                    continuation.resume(throwing: error) // Resume the task awaiting continuation by having it throw an error from its suspension point.
+                }
+            }
+        }
+    }
+    
+    // Legacy load method.
     static func load(
         completion: @escaping (Result<[DailyScrum], Error>) -> Void // Result will be an array of scrums (success) or an error (failure).
     ) {
@@ -51,6 +68,21 @@ class ScrumStore: ObservableObject { // ObservableObject: class-constrained prot
         }
     }
     
+    // Async interface for legacy save method.
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedThrowingContinuation({ continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .success(let numberOfScrumsSaved):
+                    continuation.resume(returning: numberOfScrumsSaved)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        })
+    }
+    
+    // Legacy save method.
     static func save(
         scrums: [DailyScrum],
         completion: @escaping (Result<Int, Error>) -> Void // Result will be the number of saved scrums (success) or an error (failure).
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -16,23 +16,38 @@ struct ScrumdingerApp: App {
             NavigationView {
                 ScrumsView(scrums: $store.scrums) {
                     // Save action.
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
+                    Task { // Asynchronus context (work).
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            fatalError("Unable to save scrums.")
                         }
                     }
+                    
+//                    ScrumStore.save(scrums: store.scrums) { result in
+//                        if case .failure(let error) = result {
+//                            fatalError(error.localizedDescription)
+//                        }
+//                    }
                 }
             }
-            .onAppear {
-                ScrumStore.load { result in
-                    switch result {
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    }
+            .task { // Asynchronus task to perform before view appears.
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    fatalError("Unable to load scrums.")
                 }
             }
+//            .onAppear {
+//                ScrumStore.load { result in
+//                    switch result {
+//                    case .success(let scrums):
+//                        store.scrums = scrums
+//                    case .failure(let error):
+//                        fatalError(error.localizedDescription)
+//                    }
+//                }
+//            }
         }
     }
 }
